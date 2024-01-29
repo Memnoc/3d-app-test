7b10516975dd58de975969af02a49899616f1492
Memnoc <smartdroidesign@gmail.com>
2024-01-29 06:45:20 +0000

    fixed model colors and alignment

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import "./App.css";
 import { Model } from "../public/Shoe.jsx";
-import { useState, useRef } from "react";
+import { useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import * as THREE from "three";
 import { OrbitControls, useGLTF } from "@react-three/drei";
 
 function App() {
+  // laces mesh caps inner sole striped band patch
   const [color, setColor] = useState({
     box1: "#f6b73",
     box2: "#ff0000",
     box3: "#0000ff",
     box4: "#0000ff",
+    box5: "#0000ff",
+    box6: "#0000ff",
+    box7: "#0000ff",
+    box8: "#0000ff",
   });
-
-  function handleColorChange(box) {
-    return function (event) {
+  type InputEvent = React.ChangeEvent<HTMLInputElement>;
+  function handleColorChange(box: string) {
+    return function (event: InputEvent) {
       setColor({
         ...color,
         [box]: event.target.value,
@@ -45,7 +50,16 @@ function App() {
                     castShadow
                   />
                   <Model
-                    colorPicker={{ mesh: color, caps: color, laces: color }}
+                    colorPicker={{
+                      mesh: color,
+                      laces: color,
+                      caps: color,
+                      inner: color,
+                      sole: color,
+                      stripes: color,
+                      band: color,
+                      patch: color,
+                    }}
                   />
                 </Suspense>
               </Canvas>
@@ -62,17 +76,6 @@ function App() {
                 />
                 <label htmlFor="head">Mesh</label>
               </div>
-
-              <div>
-                <input
-                  type="color"
-                  id="head"
-                  name="box1"
-                  value={color.box4}
-                  onChange={handleColorChange("box4")}
-                />
-                <label htmlFor="head">Inner</label>
-              </div>
               <div>
                 <input
                   type="color"
@@ -81,7 +84,7 @@ function App() {
                   value={color.box2}
                   onChange={handleColorChange("box2")}
                 />
-                <label htmlFor="body">Stripes</label>
+                <label htmlFor="head">Caps</label>
               </div>
               <div>
                 <input
@@ -91,7 +94,57 @@ function App() {
                   value={color.box3}
                   onChange={handleColorChange("box3")}
                 />
+                <label htmlFor="body">Inner</label>
+              </div>
+              <div>
+                <input
+                  type="color"
+                  id="head"
+                  name="box4"
+                  value={color.box4}
+                  onChange={handleColorChange("box4")}
+                />
                 <label htmlFor="body">Sole</label>
+              </div>
+              <div>
+                <input
+                  type="color"
+                  id="head"
+                  name="box5"
+                  value={color.box5}
+                  onChange={handleColorChange("box5")}
+                />
+                <label htmlFor="body">Stipes</label>
+              </div>
+              <div>
+                <input
+                  type="color"
+                  id="head"
+                  name="box6"
+                  value={color.box6}
+                  onChange={handleColorChange("box6")}
+                />
+                <label htmlFor="body">Laces</label>
+              </div>
+              <div>
+                <input
+                  type="color"
+                  id="head"
+                  name="box7"
+                  value={color.box7}
+                  onChange={handleColorChange("box7")}
+                />
+                <label htmlFor="body">Band</label>
+              </div>
+              <div>
+                <input
+                  type="color"
+                  id="head"
+                  name="box8"
+                  value={color.box8}
+                  onChange={handleColorChange("box8")}
+                />
+                <label htmlFor="body">Patch</label>
               </div>
             </div>
           </div>
